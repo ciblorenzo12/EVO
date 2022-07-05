fa35d2e653be1945f087d6659f457245c86945e6
Giovanny Castro Rosa <GACastroRosa@student.fullsail.edu>
2022-07-05 19:14:32 -0400

    working on the event pop up menu

--- app/src/main/java/com/evopackage/evo/create_event_popup.java
+++ app/src/main/java/com/evopackage/evo/create_event_popup.java
@@ -1,4 +1,29 @@
 package com.evopackage.evo;
 
-public class create_event_popup {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class create_event_popup extends AppCompatActivity {
+
+
+    private Button button;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_dialog);
+        button.setOnClickListener(v -> {
+            //openDialog();
+        });
+
+    }
+
+
 }
