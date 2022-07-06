9eaec5ca2d1ab8ee00614f83f7cc08138b5d9155
Brandon Shaffer <shafferbrandon10@hotmail.com>
2022-07-05 20:49:52 -0400

    Created profile page and profile settings layout

--- app/src/main/java/com/evopackage/evo/User_Profile_picture.java
+++ app/src/main/java/com/evopackage/evo/User_Profile_picture.java
@@ -2,13 +2,40 @@ package com.evopackage.evo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class User_Profile_picture extends AppCompatActivity {
+public class User_Profile_picture extends AppCompatActivity implements View.OnClickListener {
+private TextView _userID;
+private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile_picture);
+        setContentView(R.layout.activity_user_profile_setting);
+
+        User_information info;
+        button.findViewById(R.id.textview1000);
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.register_back3){
+            startActivity(new Intent(this, MainWindows_Create_Join_Event.class));
+        }
+        //save changes
+        else if(view.getId() == R.id.button6){
+
+        }
+        //change profile pic
+        else if(view.getId() == R.id.imageButton){
+
+        }
     }
 }
