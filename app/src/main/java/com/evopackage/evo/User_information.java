010784137fe0f894143f66a6ee19d3a9316948a5
ciblorenzo12 <ciblorenzo@gmail.com>
2022-07-07 03:30:57 -0400

    changing some rules on AndroidManifest.xml

--- app/src/main/java/com/evopackage/evo/User_information.java
+++ app/src/main/java/com/evopackage/evo/User_information.java
@@ -4,10 +4,7 @@ public class User_information {
 
 public String _firstname,_lastname,_dob,_email,_password,_phone;
 
-public User_information(){
-
-}
-public User_information( String _Firstname,String _Lastname,String _Dob,String _Email,String _Password,String _Phone){
+    public User_information( String _Firstname,String _Lastname,String _Dob,String _Email,String _Password,String _Phone){
 
     _firstname = _Firstname;
     _lastname  = _Lastname;
