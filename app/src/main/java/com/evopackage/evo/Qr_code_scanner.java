d6596aef8f7b25d7cfedf7ba77c82d49e7898932
ciblorenzo12 <ciblorenzo@gmail.com>
2022-07-18 11:25:13 -0400

    Adding Events to the user when  Qr is scanned i got scared

--- app/src/main/java/com/evopackage/evo/Qr_code_scanner.java
+++ app/src/main/java/com/evopackage/evo/Qr_code_scanner.java
@@ -11,13 +11,22 @@ import androidx.core.app.ActivityCompat;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.Result;
 
 public class Qr_code_scanner extends AppCompatActivity {
     private CodeScanner mCodeScanner;
     private MainWindows_Create_Join_Event mw;
     private  String[]perm = { Manifest.permission.CAMERA};
+    private FirebaseUser me = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference firebaseEvent = FirebaseDatabase.getInstance().getReference().child("events");
+    private DatabaseReference firebaseUser = FirebaseDatabase.getInstance().getReference().child("users");
      private TextView tw ;
+
+   private static Result _result;
 private CodeScannerView scannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,9 @@ tw = findViewById(R.id.text_uni3);
                     @Override
                     public void run() {
                     tw.setText( result.getText().trim(), TextView.BufferType.NORMAL);
+                  _result = result;
+                   firebaseEvent.child(_result.toString()).child("Users").child(me.getUid()).setValue("Guest");
+                  firebaseUser.child(me.getUid()).child("Events").child(_result.toString()).setValue("Guest");
                     }
                 });
             }
@@ -69,6 +81,14 @@ tw = findViewById(R.id.text_uni3);
         mCodeScanner.releaseResources();
 
         super.onPause();
+    }
+    public static String GetScan(){
+
+
+
+
+
+        return _result.getText() ;
     }
 
 }
