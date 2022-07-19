44421f01c6f86537ca4fa7d9b134859c12faccd1
ciblorenzo12 <ciblorenzo@gmail.com>
2022-07-19 16:40:50 -0400

    adding stuff

--- app/src/main/java/com/evopackage/evo/Login.java
+++ app/src/main/java/com/evopackage/evo/Login.java
@@ -29,6 +29,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
+
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     //google
@@ -199,7 +200,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                     if (task.isSuccessful()) {
 
                         UI_Update();
-                  User_information data = new User_information(_authent.getCurrentUser().getDisplayName()," "," ", _authent.getCurrentUser().getEmail()," ",_authent.getCurrentUser().getPhoneNumber(),"Events");
+                  User_information data = new User_information(_authent.getCurrentUser().getDisplayName()," "," ", _authent.getCurrentUser().getEmail()," ",_authent.getCurrentUser().getPhoneNumber(),"Events"," ");
                         _database.getReference("users").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser())
                                 .getUid()).setValue(data);
                     }
