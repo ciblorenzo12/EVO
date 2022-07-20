d78c3da09613909a93b532364ae30d527e9f1a1a
Brandon Shaffer <shafferbrandon10@hotmail.com>
2022-07-19 20:04:27 -0400

    Commit

---
+++ app/src/main/java/com/evopackage/evo/Event_History.java
@@ -0,0 +1,14 @@
+package com.evopackage.evo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Event_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_history);
+    }
+}
