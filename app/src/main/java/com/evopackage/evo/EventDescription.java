b73a65b04b166f3a08c02d1485ea459c74c8f539
Riley Paris <RWParis@student.fullsail.edu>
2022-08-09 16:13:35 -0400

    cleanup, event class modify, and attendee counter

--- app/src/main/java/com/evopackage/evo/EventDescription.java
+++ app/src/main/java/com/evopackage/evo/EventDescription.java
@@ -3,20 +3,21 @@ package com.evopackage.evo;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.evopackage.evo.databinding.EventDescriptionBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class EventDescription extends AppCompatActivity {
 
     private EventDescriptionBinding binding;
     private Event ev;
-    private TextView txtName;
-    private TextView txtAddress;
-    private TextView txtDate;
-    private TextView txtCategory;
-    private TextView txtDescription;
-    private TextView txtCreator;
+    private TextView txtName, txtAddress, txtDate, txtCategory, txtDescription, txtCreator, txtAttendees;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,7 @@ public class EventDescription extends AppCompatActivity {
         txtCategory = findViewById(R.id.category4item);
         txtDescription = findViewById(R.id.descriptionev);
         txtCreator = findViewById(R.id.eventcreatorr);
+        txtAttendees = findViewById(R.id.txtAttendees);
 
         populateData();
     }
@@ -44,5 +46,16 @@ public class EventDescription extends AppCompatActivity {
         txtCategory.setText(ev.GetCategory());
         txtDescription.setText(ev.GetDescription());
         txtCreator.setText(ev.GetCreator());
+        FirebaseDatabase.getInstance().getReference().child("events").child(ev.GetKey()).child("assistance").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                txtAttendees.setText(snapshot.getChildrenCount() + " Attendees");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
